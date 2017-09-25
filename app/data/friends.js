9959557cfa1076be0c6f19a3bda9d0acd5f7eeef
David Waterman <davidwaterman@Davids-MacBook-Air.local>
2017-09-24 20:56:17 -0500

    a

---
+++ app/data/friends.js
@@ -0,0 +1,83 @@
+var friendsArray = [{
+        name: "Rick",
+        photo: "/assets/images/Rick.png",
+        scores: [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "1",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Morty",
+        photo: "/assets/images/morty.png",
+        scores: [
+            "1",
+            "4",
+            "2",
+            "1",
+            "5",
+            "3",
+            "3",
+            "4",
+            "3",
+            "5"
+        ]
+    },
+    {
+        name: "Mr.Poopy Butthole",
+        photo: "/assets/images/mrpbh.png",
+        scores: [
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3"
+        ]
+    },
+    {
+        name: "Ants in my Eyes Johnson",
+        photo: "/assets/images/ants.png",
+        scores: [
+            "1",
+            "5",
+            "1",
+            "5",
+            "1",
+            "5",
+            "1",
+            "5",
+            "1",
+            "5"
+        ]
+    },
+    {
+        name: "Birdperson",
+        photo: "/assets/images/Birdperson.png",
+        scores: [
+            "4",
+            "5",
+            "3",
+            "3",
+            "3",
+            "4",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
+    }
+];
+
+module.exports = friendsArray;
